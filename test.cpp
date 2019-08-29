ea9a90ad0889555916a5a0b8a4042ec742a6a83f
Ignacio Ricart <ignacio@poli.ufrj.br>
2019-08-29 15:19:33 -0300

    Add class boladona

--- test.cpp
+++ test.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
+class Boladona
+{
+public:
+	Boladona()
+	{
+		std::cout << "Sou boladona!" << std::endl;
+	}
+}
+
+
 int main()
 {
+	Boladona bolei;
 	std::cout << "Teste cpp" << std::endl;
 	return 0;
 }
